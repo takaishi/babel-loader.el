c56e90999dfe6b815a390f9c233130ac34ba9115
Ryo TAKAISHI <ryo.takaishi.0@gmail.com>
2012-08-25 14:37:52 +0900

    fix typo

--- babel-loader.el
+++ babel-loader.el
@@ -38,7 +38,7 @@
 (defun bl:compile-dir (dir)
   (mapc #'(lambda (file)
 			(let* ((base-name (file-name-sans-extension file))
-				   (exported-file (concat base-name ".elq")))
+				   (exported-file (concat base-name ".el")))
 			  (bl:compile file exported-file)))
 		(directory-files dir t "\\.org$")))
 
