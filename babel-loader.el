38f8eee12250eb49c5c5a5a010b1ad005190785c
Ryo TAKAISHI <ryo.takaishi.0@gmail.com>
2012-08-25 12:22:39 +0900

    compile org-file to el-file in directory.

--- babel-loader.el
+++ babel-loader.el
@@ -6,3 +6,9 @@
 						 exported-file
 						 "emacs-lisp"))
 
+(defun bl:compile-dir (dir)
+  (mapc #'(lambda (file)
+			(let* ((base-name (file-name-sans-extension file))
+				   (exported-file (concat base-name ".el")))
+			  (bl:compile file exported-file)))
+		(directory-files dir t "\\.org$")))
